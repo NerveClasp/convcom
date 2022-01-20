008f2767129b4f36a69a6819ad3a1f6600c0d9ab
Roman Horokhov <nerveclasp@gmail.com>
2022-01-20 20:51:08 +0200

    feat: stream 2: getting branch name and message argument (NC-0)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 use regex::Regex;
+use std::env;
+use std::io::Error;
+use std::process::exit;
+use std::process::Command;
 
-fn main() {
-    let text = String::from("feature/BC-123-yo-stream");
-    let test_message_raw = "Test commit message";
+fn main() -> Result<(), Error> {
+    let args: Vec<String> = env::args().collect();
+    let commit_message = args[1].clone();
+    println!("{:?}", commit_message);
+    let output = Command::new("git")
+        .arg("branch")
+        .arg("--show-current")
+        .output()?;
+    if !output.status.success() {
+        println!("Could not get current git branch name! Are you in a git repo right now?");
+        exit(1);
+    }
+    let branch_name = String::from_utf8(output.stdout).unwrap();
+    println!("{:?}", branch_name.trim());
     // (\w+)\/([A-Z]*-?\d+)
     let type_re = Regex::new(r"^(\w+)").unwrap(); // TODO: figure out something better
     let ticket_re = Regex::new(r"([A-Z]*-?\d+)").unwrap();
-    let prefix = type_re.find(&text).unwrap().as_str();
-    let ticket_number = ticket_re.find(&text).unwrap().as_str();
+    let prefix = type_re.find(&branch_name).unwrap().as_str();
+    let ticket_number = ticket_re.find(&branch_name).unwrap().as_str();
     let message_type = match prefix {
         "feature" => "feat",
         "feat" => "feat",
@@ -16,11 +31,25 @@ fn main() {
         "quickfix" => "fix",
         _ => "",
     };
-    let mut v: Vec<char> = test_message_raw.chars().collect();
+    let mut v: Vec<char> = commit_message.chars().collect();
     v[0] = v[0].to_lowercase().nth(0).unwrap();
     let test_message: String = v.into_iter().collect();
 
     let result_string = format!("{}: {} ({})", message_type, test_message, ticket_number);
 
     println!("Result: {}", result_string);
+    let final_result = Command::new("git")
+        .arg("commit")
+        .arg("-m")
+        .arg(result_string)
+        .output()?;
+
+    if !final_result.status.success() {
+        let error_message = String::from_utf8(final_result.stdout).unwrap();
+
+        println!("Error: {}", error_message);
+        exit(1);
+    }
+
+    Ok(())
 }
